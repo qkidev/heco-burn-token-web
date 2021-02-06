53f468ec4b8b909ca05e816c60ab0ebc7fc63506
wxyforgithub <wxyforgithub@163.com>
2021-02-06 15:41:56 +0800

    heco燃烧挖矿

---
+++ src/utils/global.js
@@ -0,0 +1,15 @@
+const HT_CONFIG = {
+  chainId: '20181205',
+  chainIdHex: '0x133f0d5',
+  tipsDesc: '推荐使用qkswap交换bt和其它通证，避免被骗。  qkswap可以点击进入',
+  tipsUrl: 'https://app.qkswap.io',
+  openPluginToast : '请安装metamask插件、或者使用qkpay打开',
+  toggleToast : '请使用QKI主网,请切换到QKI主网',
+  useToast: '请使用qki主网',
+  toggleToast2: '你当前没有使用QKI主网，请切换主网为QKI',
+  contractAdress: '0x3FB708e854041673433e708feDb9a1b43905b6f7'
+}
+const GLOBAL_CONFIGS = HT_CONFIG
+export {
+  GLOBAL_CONFIGS
+};
